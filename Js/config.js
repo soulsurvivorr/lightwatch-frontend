3fd89391ec84021925b52374ff9c1e477b8d60b0
soulsurvivor <koffibahd16@gmail.com>
2026-06-25 19:11:18 +0000

    allow scroll but no bounce

--- Js/config.js
+++ Js/config.js
@@ -1,11 +1,2 @@
 const API_URL = "https://lightwatch-backend.onrender.com";
 
-
-// Prevent default touch behavior (shake/bounce)
-document.addEventListener('touchmove', function(e) {
-  e.preventDefault();
-}, { passive: false });
-
-document.addEventListener('touchstart', function(e) {
-  e.preventDefault();
-}, { passive: false });
