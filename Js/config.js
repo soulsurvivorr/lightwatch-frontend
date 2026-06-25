957a41d4448e1e285d23fd07e76d68ea53732815
soulsurvivor <koffibahd16@gmail.com>
2026-06-25 19:05:53 +0000

    added js to stop the screen from shrinking when touched

--- Js/config.js
+++ Js/config.js
@@ -1,1 +1,11 @@
 const API_URL = "https://lightwatch-backend.onrender.com";
+
+
+// Prevent default touch behavior (shake/bounce)
+document.addEventListener('touchmove', function(e) {
+  e.preventDefault();
+}, { passive: false });
+
+document.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+}, { passive: false });
