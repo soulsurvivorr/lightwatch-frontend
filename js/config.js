169606401b0050204e7759a392c0f00446b1563d
soulsurvivor <koffibahd16@gmail.com>
2026-08-02 17:54:59 +0000

    backend hoster changed

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
 // ============================================================
 
 // Production backend URL - used by all platforms (Web & Android)
-const API_URL = "https://lightwatch-backend-1yko.onrender.com";
+const API_URL = "https://lightwatch-backend-lightwatch-backend.up.railway.app";
 
 console.log("[Config] API_URL set to:", API_URL);
